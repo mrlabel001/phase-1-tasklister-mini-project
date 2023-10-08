a2be5a767268ac7fb68b1e3cf42a5df353f79782
mrlabel001 <farbean.makini@student.moringaschool.com>
2023-10-08 13:21:17 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let btn = document.querySelector("#create")
+btn.addEventListener("submit", (e)=> {
+  e.preventDefault()
+  let list = e.target.new.value;
+  let p = document.createElement("p")
+  let container = document.querySelector("#list")
+  p.textContent = list
+  container.appendChild(p)
+  btn.reset()
+})
